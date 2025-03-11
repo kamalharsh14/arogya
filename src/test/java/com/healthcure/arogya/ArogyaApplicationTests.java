5c9baad74fd2d2be53a6ccce7c98f493341c69e5
Harsh Kamal Singh <singhharsh998@gmail.com>
2025-03-11 09:11:57 +0530

    initial commit

---
+++ src/test/java/com/healthcure/arogya/ArogyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcure.arogya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArogyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
